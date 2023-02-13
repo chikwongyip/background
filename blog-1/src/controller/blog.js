ef74138c7e19bd5edc8238e07b08bc81e091319d
chikwongyip <527439024@qq.com>
2023-02-13 15:27:28 +0800

    开发blog路由表

---
+++ blog-1/src/controller/blog.js
@@ -0,0 +1,23 @@
+const getList = (author,keyword) => {
+    // 先返回假数据，（格式是正确的）
+    return [
+        {
+            id:1,
+            title:"标题",
+            content:"dasdas123asd1312312312",
+            createTime:1676271581651,
+            author:"zhangsan"
+        },
+        {
+            id:2,
+            title:"标题2",
+            content:"das3123asddas123asd1312312312",
+            createTime:1676271625415,
+            author:"lisi"
+        }
+    ]
+}
+
+module.exports = {
+    getList
+}
--- blog-1/src/router/blog.js
+++ blog-1/src/router/blog.js
@@ -1,13 +1,15 @@
+const { getList } = require("../controller/blog")
+const { SuccessModel, ErrorModel } = require("../model/resModel")
+
 const handleBlogRouter = (req, res) => {
     const method = req.method; //GET or POST
-    const url = req.url;
-    const path = url.split("?")[0];
-
+    const path = req.url.split("?")[0]
     //获取blog 列表
     if(method === "GET" && path === "/api/blog/list"){
-        return{
-            msg:"这是获取blog列表的接口"
-        }
+        const author = req.query.author || ""
+        const keyword = req.query.keyword || ""
+        const listData = getList(author,keyword)
+        return new SuccessModel(listData)
     }
 
     //获取blog详情
