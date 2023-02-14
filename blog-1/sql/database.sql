826f09355f41be94ebcb0ba19d549bcf5d8c917c
chikwongyip <527439024@qq.com>
2023-02-15 07:32:34 +0800

    sql

---
+++ blog-1/sql/database.sql
@@ -0,0 +1,19 @@
+create schema myblog
+create table if not exists blogs
+(
+    id         int auto_increment
+        primary key,
+    title      varchar(50)      not null,
+    content    longtext         not null,
+    createtime bigint default 0 not null,
+    author     varchar(20)      not null
+);
+
+create table if not exists users
+(
+    id       int auto_increment
+        primary key,
+    username varchar(20) not null,
+    password varchar(20) not null,
+    realname varchar(10) not null
+);
