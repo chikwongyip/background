561061f31f37c4e163106d4192cd9609e74c48f3
chikwongyip <527439024@qq.com>
2023-02-15 08:12:06 +0800

    mysql connection

---
+++ sqldemo/index.js
@@ -0,0 +1,21 @@
+const mysql = require("mysql")
+// 创建对象
+const con = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"528478huaHUA@",
+    port:"3306",
+    database:"myblog"
+})
+// 开始连接
+con.connect()
+// 执行sql 语句
+const sql = "select * from users;"
+con.query(sql,(err,result) => {
+    if(err){
+        console.error(err)
+        return
+    }
+    console.log(result)
+})
+con.end()
