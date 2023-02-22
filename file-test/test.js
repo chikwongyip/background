4644912b8bdba6c956f4457f808bc3dd9933d324
chikwongyip <527439024@qq.com>
2023-02-23 07:23:55 +0800

    stream

--- file-test/test.js
+++ file-test/test.js
@@ -2,6 +2,7 @@ const fs = require("fs")
 const path = require("path")
 
 const fileName = path.resolve(__dirname,"data.txt")
+const bkFileName = path.resolve(__dirname,"data_bk.txt")
 // fs.readFile(fileName,(err,data) => {
 //     if(err){
 //         console.log(err)
@@ -10,22 +11,31 @@ const fileName = path.resolve(__dirname,"data.txt")
 //     console.log(data.toString())
 // })
 
-const content = "\n这是要写入的内容\n"
-const opt = {
-    append:{
-        endcoding:"utf8",
-        mode:0o666,
-        flag:'a'
-    },
-    overwirte:{
-        endcoding:"utf8",
-        mode:0o666,
-        flag:'w'
-    }
-}
-fs.writeFile(fileName,content,opt.append,(err) => {
-    if(err){
-        console.log(err)
-    }
-    return
+// const content = "\n这是要写入的内容\n"
+// const opt = {
+//     append:{
+//         endcoding:"utf8",
+//         mode:0o666,
+//         flag:'a'
+//     },
+//     overwirte:{
+//         endcoding:"utf8",
+//         mode:0o666,
+//         flag:'w'
+//     }
+// }
+// fs.writeFile(fileName,content,opt.append,(err) => {
+//     if(err){
+//         console.log(err)
+//     }
+//     return
+// })
+let readStream = fs.createReadStream(fileName)
+let writeStream = fs.createWriteStream(bkFileName)
+readStream.pipe(writeStream)
+readStream.on("data",chunk => {
+    console.log(chunk.toString())
+})
+readStream.on("end",() =>{
+    console.log("写入完毕")
 })
---
+++ stream-test/test-1.js
@@ -0,0 +1,8 @@
+// process.stdin.pipe(process.stdout)
+const http = require("http")
+const server = http.createServer((req,res) => {
+    if(req.method === "POST"){
+        req.pipe(res)
+    }
+})
+server.listen(8002)
