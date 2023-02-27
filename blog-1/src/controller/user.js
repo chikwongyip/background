5829582c99be28035a2a25ce3576eece1ad55945
chikwongyip <527439024@qq.com>
2023-02-27 21:22:10 +0800

    密码加密

--- blog-1/src/controller/user.js
+++ blog-1/src/controller/user.js
@@ -1,11 +1,14 @@
 const { exec,escape } = require("../db/mysql")
+const { genPassword } = require("../utils/cryp")
 
 const login = (username,password) => {
     username = escape(username)
+    // 生成加密密码
+    password = genPassword(password)
     password = escape(password)
     const sql = `
         select username,realname from users where username = ${username}
-                                            and password = ${password}
+                                              and password = ${password}
     `
     return exec(sql).then(data => {
         return data[0] || {}
