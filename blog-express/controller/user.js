26973cd9972c7c34a08248fed0b8e9f5ed448185
chikwongyip <527439024@qq.com>
2023-03-04 08:09:50 +0800

    client 端不能正常显示页面

--- blog-express/controller/user.js
+++ blog-express/controller/user.js
@@ -4,7 +4,7 @@ const { genPassword } = require("../utils/cryp")
 const login = (username,password) => {
     username = escape(username)
     // 生成加密密码
-    password = genPassword(password)
+    //password = genPassword(password)
     password = escape(password)
     const sql = `
         select username,realname from users where username = ${username}
